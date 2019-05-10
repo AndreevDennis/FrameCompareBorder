d48eb5f9db7c4a44499fa98ac35adcc3277a8efa
Dennis <denisandreev1611@gmail.com>
2019-05-10 20:12:42 +0300

    add Sliders

--- FrameCompareBorder/ViewController.swift
+++ FrameCompareBorder/ViewController.swift
@@ -10,9 +10,37 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var frameX: UILabel!
+    @IBOutlet weak var frameY: UILabel!
+    @IBOutlet weak var frameWidth: UILabel!
+    @IBOutlet weak var frameHeight: UILabel!
+    @IBOutlet weak var boundsX: UILabel!
+    @IBOutlet weak var boundsY: UILabel!
+    @IBOutlet weak var boundsHeight: UILabel!
+    @IBOutlet weak var boundsWidth: UILabel!
+    @IBOutlet weak var rotation: UILabel!
+    @IBOutlet weak var centerY: UILabel!
+    @IBOutlet weak var centerX: UILabel!
+    
+    
+    
+    
+    @IBOutlet weak var frameXSlider: UISlider!
+    @IBOutlet weak var frameYSlider: UISlider!
+    @IBOutlet weak var frameWidthSlider: UISlider!
+    @IBOutlet weak var frameHeightSlider: UISlider!
+    @IBOutlet weak var boundsXSlider: UISlider!
+    @IBOutlet weak var boundsYSlider: UISlider!
+    @IBOutlet weak var boundsWidthSlider: UISlider!
+    @IBOutlet weak var boundsHeightSlider: UISlider!
+    @IBOutlet weak var centerXSlider: UISlider!
+    @IBOutlet weak var rotationSlider: UISlider!
+    @IBOutlet weak var centerYSlider: UISlider!
+    
+    
+    
     override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
     }
 
 
